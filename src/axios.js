7b6f91f923daa8c1acd2ce99c546ea1cb25299ea
user <danil369.ru@mail.ru>
2023-02-08 19:32:33 +0600

    API

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 // https://www.digitalocean.com/community/tutorials/react-axios-react
-const BASE_URL = 'https://danil369.pythonanywhere.com/'
+const BASE_URL = '/api/'
 export default axios.create({
   baseURL: BASE_URL,
 });
